5fe1516585025228beb72741995296e655c672ff
GabrielLopez <gmlopez11@espe.edu.ec>
2025-05-16 22:20:40 -0500

    Database Update

--- 06-Code/PHP/profile_data_register.php
+++ 06-Code/PHP/profile_data_register.php
@@ -2,10 +2,26 @@
 include 'connection_db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $
+    $name = misqlysqli_real_escape_string($connection, $_POST['name']);
+     $permisos = $_POST["permisos"]; 
 
-    // Prepare and bind
+    $permisosCampos = [
+        "lectura_ingresos", "Insercion_ingresos", "edicion_ingresos",
+        "lectura_gastos", "insercion_gastos", "edicion_gastos",
+        "lectura_usuarios", "insercion_usuarios", "edicion_usuarios",
+        "lectura_perfiles", "insercion_perfiles", "edicion_perfiles",
+        "lectura_conceptos", "insercion_conceptos", "edicion_conceptos",
+        "permiso_qr"
+    ];
+    
+
+     $valores = [];
+    foreach ($permisosCampos as $permiso) {
+        $valores[$permiso] = in_array($permiso, $permisos) ? "TRUE" : "FALSE";
+    }
+
+    $sql = "INSERT INTO perfiles (perfil, " . implode(", ", array_keys($valores)) . ") 
+            VALUES ('$nombrePerfil', " . implode(", ", $valores) . ")";
     $stmt = $connection->prepare("INSERT INTO users (name, email, password, phone, address) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param("sssss", $name, $email, $password, $phone, $address);
 
