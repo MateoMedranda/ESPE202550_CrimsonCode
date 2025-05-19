9aca7e0bcc290b15f042eb24c10e6400c1d6afec
unknown <matemedranda15@gmail.com>
2025-05-19 17:18:53 -0500

    1.5.0

--- 06-Code/JS/project_managment.js
+++ 06-Code/JS/project_managment.js
@@ -3,6 +3,9 @@ const modal_add_project = document.getElementById('modal_add_project');
 const btn_cancel_add_project = document.getElementById('btn_cancel_add_project');
 const project_image_file = document.getElementById('project_image_input');
 const preview_image_box = document.getElementById('image_preview');
+let  project_full_list = [];
+
+get_full_project_list();
 
 btn_add_project.addEventListener('click', () => {
     modal_add_project.showModal(); 
@@ -41,3 +44,57 @@ project_image_file.addEventListener('change', (event) => {
 });
 
 
+function get_full_project_list(){
+    fetch("../PHP/project_managment/project_full_list.php")
+    .then(response => response.json())
+    .then(data => {
+        console.log("Projects were catch:", data);
+        if (data.error) {
+            console.error("[ERROR load_fill_project_list]: ", data.error);
+        } else {
+            project_full_list = data;
+            load_full_project_list();
+        }
+    })
+    .catch(error => console.error("Error en la solicitud fetch:", error));
+}
+
+function load_full_project_list(){
+    let project_content_div = document.getElementById("project_content_div");
+    let string_divs = "";
+    project_full_list.forEach((project) =>{
+        let new_div_project = `
+            <div class="col-md-4 col-sm-12 mt-4 mb-4">
+                    <div class="card-proyecto col-md-10 col-sm-12 m-auto rounded p-0">
+                        <div class="px-2 pt-2">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="div_project_title" onclick="open_project(${project.id})">
+                                    <h5 class="mb-0 title_project fw-bold">${project.name}</h5>
+                                    <hr class="m-0">
+                                </div>
+
+                                <!-- Dropdown project -->
+                                <div class="dropdown">
+                                    <div class="project_options rounded" data-bs-toggle="dropdown" aria-expanded="false"
+                                        role="button">
+                                        <h2 class="mb-0"><i class="bi bi-list"></i></h2>
+                                    </div>
+                                    <ul class="dropdown-menu dropdown-menu-end">
+                                        <li><a class="dropdown-item" onclick="update_project(${project.id})">Editar</a></li>
+                                        <li><a class="dropdown-item" onclick="delete_project(${project.id})">Eliminar</a></li>
+                                        <li><a class="dropdown-item" onclick="open_project(${project.id})">Ver detalles</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <h6 class="mt-2">Inicio: ${project.begin_date}</h6>
+                        </div>
+                        <div class="div_project_image w-100 d-sm-none d-md-block" onclick="open_project(${project.id})"><img src="../${project.image}" width="100%"></div>
+                    </div>
+                </div>
+            `;
+
+        string_divs +=new_div_project;
+    });
+    project_content_div.innerHTML = string_divs;
+}
---
+++ 06-Code/PHP/project_managment/project_full_list.php
@@ -0,0 +1,12 @@
+<?php
+include("../connection_db.php");
+    $request = 'SELECT p.PROJECT_ID AS "id" p.PROJECT_NAME AS "name" , p.PROJECT_STARTDATE AS "begin_date" p.PROJECT_IMAGE AS "image" FROM PROJECT p';
+    $result = mysqli_query($connection,$request);
+
+    if($result){
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        echo json_encode($data);
+    }else {
+        echo json_encode(["[ERROR]" => "There is an error in the request for project list"]);
+    }
+?>
