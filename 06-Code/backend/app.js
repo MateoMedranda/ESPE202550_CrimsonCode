fc4417d5b9657d306910318e7e6475ee5bbf4d6d
Gabriel Matias López Guano <141784630+Gabwr@users.noreply.github.com>
2025-06-17 20:59:44 -0500

    Static frontendConfiguration app.js

--- 06-Code/backend/app.js
+++ 06-Code/backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const profileRoutes = require('./routes/profileRoutes');
 
@@ -7,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../06-Code')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../06-Code/index.php')); 
+});
 
 app.use('/api/profiles', profileRoutes);
 
